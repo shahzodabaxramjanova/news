9567cd95f96dbc0bdce16030f7eec68a8c4a3da6
shahzodabaxramjanova <bdshahzoda@gmail.com>
2026-05-31 00:52:52 +0500

    add requirements

--- news_project/settings.py
+++ news_project/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure--9d!qx)gk-c!xo-qm07h8a-^r*6096xty_y0z%-@_cvl2o0-k1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".vercel.app",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
